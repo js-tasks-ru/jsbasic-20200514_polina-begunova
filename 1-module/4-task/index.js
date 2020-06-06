ce660f04a67277e840936458d898fd0e5f8a2550
Polina Begunova <polina.begunova@yandex.ru>
2020-06-06 23:01:44 +0300

    Проверка на спам

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -4,5 +4,11 @@
  * @returns {boolean}
  */
 function checkSpam(str) {
+let lowerStr = str.toLowerCase();
 
+  return lowerStr.includes('1xbet') || lowerStr.includes('xxx');
 }
+
+alert( checkSpam('1XbeT now') );
+alert( checkSpam('free xxxxx') );
+alert( checkSpam('innocent rabbit') );
